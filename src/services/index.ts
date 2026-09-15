22e3ccb08423c24abc9b13c6c04984c71cd5092c
Ihimbazwe Samuel <ihimbazwe34@gmail.com>
2026-09-15 11:38:48 +0200

    feat: update services index

--- src/services/index.ts
+++ src/services/index.ts
@@ -1,20 +1,27 @@
+export { boardService } from './boardService';
+export { oversightReportsService } from './oversightReportsService';
 export { musicScheduleService } from './musicScheduleService';
 export { ministryFinanceService } from './ministryFinanceService';
 export { accessService } from './accessService';
+export { attentionService } from './attentionService';
 export { auditService } from './auditService';
 export { authService, peopleService } from './authService';
 export { choirService } from './choirService';
+export { choirContributionOps } from './choirContributionOps';
 export {
   getActiveChoirOrgUnitId,
   requireActiveChoirOrgUnitId,
   setActiveChoirOrgUnitId,
   syncChoirScopeFromSession,
 } from './choirScope';
-export { deaconService } from './deaconService';
+export { pastoralOpsService } from './pastoralOpsService';
 export { churchFinanceService } from './churchFinanceService';
+export { deaconService } from './deaconService';
 export { financeService } from './financeService';
 export { worshipService } from './worshipService';
-export { missionService, isChurchLeadership } from './missionService';
+export { missionService, isChurchLeadership, isChurchLeader } from './missionService';
+export { reportsService, downloadText } from './reportsService';
+export { reportPrefs } from './reportPrefs';
 export { orgService, systemsService } from './orgService';
 export { participationService } from './participationService';
 export { protocolService } from './protocolService';
